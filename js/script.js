2ff99fc1ad18c28f724ac73b2d3b8dd6439e6a7d
wniebelski <wniebelski@gmail.com>
2022-11-14 14:26:53 +0100

    warsztat

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log ('WARSZAT - Obsługa formularza "Imię i Nazwisko"');
+
+const submitForm = (event) => {
+    event.preventDefault ();
+
+    let firstName = document.querySelector('[name="fname"]');
+    let lastName = document.querySelector('[name="lname"]');
+    
+    console.log(`Imie: ${firstName.value}, Nazwisko: ${lastName.value}`);
+}
+
+let form = document.getElementById ('form')
+
+form.addEventListener('submit' , submitForm);
